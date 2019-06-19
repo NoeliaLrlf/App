562b3e6306a7aa616cb35d36800fec492bfdc9f8
NoeliaLrlf <cnoelia900@gmail.com>
2019-06-18 22:49:02 -0400

    qdd Signed-off-by: NoeliaLrlf <cnoelia900@gmail.com>

--- RestApi/UiApp/FeatureFiles/TestFeature.feature
+++ RestApi/UiApp/FeatureFiles/TestFeature.feature
@@ -1,11 +1,8 @@
-﻿Feature: TestFeature
+﻿Feature: TestFeature123
 	In order to avoid silly mistakes
-	As a math idiot
-	I want to be told the sum of two numbers
-
-@mytag
-Scenario: Add two numbers
-	Given I have entered 50 into the calculator
-	And I have entered 70 into the calculator
-	When I press add
-	Then the result should be 120 on the screen
+#Scenario: Incorrect password
+#Given User is log in
+#	And I click on settings
+#	And I enter incorrect password
+#	When I click Ok button
+#	Then A alert message should be displayed 
---
+++ RestApi/UiApp/StepDefinition/StepDefinition.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using UiApp.Page.DriverPage;
+using UiApp.PageObject;
+
+namespace UiApp.StepDefinition
+{
+    [Binding]
+    public sealed class StepDefinition
+    {
+        public Settings settings = new Settings();
+        #region Given 
+        [Given(@"User is log in")]
+        public void GivenUserIsLogIn()
+        {
+            LoginPage loginPage = new LoginPage();
+            loginPage.ClickLogin();
+            loginPage.EnterLoginCredentials(ConfigurationManager.AppSettings["UserName"], ConfigurationManager.AppSettings["Password"]);
+            loginPage.GoTodoPage();
+        }
+        #endregion
+        #region Then
+        [Given(@"I click on settings")]
+        public void GivenIClickOnSettings()
+        {
+            
+            settings.ClickSettings();
+        }
+
+        #endregion
+
+
+
+        #region Then
+        #endregion
+
+        #region When
+        #endregion
+        [Given(@"I enter incorrect password")]
+        public void GivenIEnterIncorrectPassword()
+        {
+            settings.EnterOldPassword();
+        }
+
+        [When(@"I click Ok button")]
+        public void WhenIClickOkButton()
+        {
+            settings.ClickOkButton();
+        }
+
+        [Then(@"A alert message should be displayed")]
+        public void ThenAAlertMessageShouldBeDisplayed()
+        {
+            settings.Alert();
+            IAlert alert = DriverPage.Instance._driver.SwitchTo().Alert();
+            var text = alert.Text;
+            alert.Accept();
+            DriverPage.Instance._driver.SwitchTo().DefaultContent();
+            Assert.AreEqual(text, "Invalid Password");
+        }
+
+
+    }
+}
