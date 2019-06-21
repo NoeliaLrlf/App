cb5560b2974bba2d044eff2fbe8999f8f1362de4
NoeliaLrlf <cnoelia900@gmail.com>
2019-06-20 22:29:04 -0400

    add
    Signed-off-by: NoeliaLrlf <cnoelia900@gmail.com>

--- RestApi/UiApp/StepDefinition/StepDefinition.cs
+++ RestApi/UiApp/StepDefinition/StepDefinition.cs
@@ -1,42 +1,79 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using TechTalk.SpecFlow;
+using UiApp.Page.DriverPage;
+using UiApp.PageObject;
 
 namespace UiApp.StepDefinition
 {
     [Binding]
     public sealed class StepDefinition
     {
+        public Settings settings;
+        #region MyRegion
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            DriverPage.Instance.StartBrowser(ConfigurationManager.AppSettings["Website"]);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            DriverPage.Instance.StopBrowser();
+        }
+
+        
+
+            #endregion
+       
+
         [Given(@"User is log in")]
         public void GivenUserIsLogIn()
         {
-            ScenarioContext.Current.Pending();
+            LoginPage loginPage = new LoginPage();
+            loginPage.ClickLogin();
+            loginPage.EnterLoginCredentials(ConfigurationManager.AppSettings["UserName"], ConfigurationManager.AppSettings["Password"]);
+            loginPage.GoTodoPage();
         }
 
         [When(@"I click on settings")]
         public void WhenIClickOnSettings()
         {
-            ScenarioContext.Current.Pending();
+            settings            = new Settings();
+            settings.ClickSettings();
         }
 
         [When(@"I enter incorrect password")]
         public void WhenIEnterIncorrectPassword()
         {
-            ScenarioContext.Current.Pending();
+            settings.EnterOldPassword();
+           
         }
 
         [When(@"I click Ok button")]
         public void WhenIClickOkButton()
         {
-            ScenarioContext.Current.Pending();
+            settings.ClickOkButton();
         }
 
         [Then(@"A alert message should be displayed")]
         public void ThenAAlertMessageShouldBeDisplayed()
         {
-            ScenarioContext.Current.Pending();
+
+
+
+            settings.Alert();
+            IAlert alert        = DriverPage.Instance._driver.SwitchTo().Alert();
+            var text            = alert.Text;
+            alert.Accept();
+            DriverPage.Instance._driver.SwitchTo().DefaultContent();
+            Assert.AreEqual(text, "Invalid Password");
         }
 
     }
